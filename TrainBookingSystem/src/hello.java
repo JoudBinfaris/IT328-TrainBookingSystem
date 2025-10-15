b7c1d36c2543f9a0275d29d6f89ffbdb8b64f7ec
JoudBinfaris <Joudbinfaris@gmail.com>
2025-10-15 22:25:45 +0300

    Delete hello.java

--- TrainBookingSystem/src/hello.java
+++
@@ -1,8 +0,0 @@
-
-public class hello {
-    public static void main(String[] args){
-   System.out.println("Can you see me?");
-   System.out.println("yes noura");
-   System.out.println("الملكه هنوف");
-    }
-}
