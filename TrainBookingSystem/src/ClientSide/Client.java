2bbedf248ee942d56504a13880366aa655d173d4
JoudBinfaris <Joudbinfaris@gmail.com>
2025-10-17 13:15:05 +0300

    client class

--- TrainBookingSystem/src/ClientSide/Client.java
+++ TrainBookingSystem/src/ClientSide/Client.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ServerSide;
+package ClientSide;
 
 /**
  *
  * @author joud
  */
-public class Server {
-    public static void main (String []args){
-        System.out.println("pls work");
-    }
-   
+public class Client {
     
 }
