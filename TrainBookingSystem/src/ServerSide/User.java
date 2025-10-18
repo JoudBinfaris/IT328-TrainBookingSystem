b913389b6044edc11e4c2b5f8a5c05c0a5fe40fe
JoudBinfaris <Joudbinfaris@gmail.com>
2025-10-18 15:52:08 +0300

    Update User.java

--- TrainBookingSystem/src/ServerSide/User.java
+++ TrainBookingSystem/src/ServerSide/User.java
@@ -9,5 +9,21 @@ package ServerSide;
  * @author joud
  */
 public class User {
+    private String username;
+    private String password;
+
+    // constructor
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // getters
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+
+   
+    
+
     
 }
